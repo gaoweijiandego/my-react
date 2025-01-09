e7f571dd255656ed4b4cd395b29e4274e715671f
NIKOFCUKINGTOP <2735977754@qq.com>
2025-01-09 10:09:35 +0800

    测试代码

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  switch (req.method) {
+    case 'GET':
+      // 处理 GET 请求
+    
+      res.status(200).json({ message: 'Hello from GET API!', name: 'joker' });
+      break;
+    case 'POST':
+      // 处理 POST 请求
+      const { name } = req.body; // 从请求体中获取数据
+      console.log(name,'dasdasda')
+      res.status(200).json({ message: `Hello, ${name}!`, name: name });
+      break;
+    case 'PUT':
+      // 处理 PUT 请求
+      res.status(200).json({ message: 'Hello from PUT API!' });
+      break;
+    case 'DELETE':
+      // 处理 DELETE 请求
+      res.status(200).json({ message: 'Hello from DELETE API!' });
+      break;
+    default:
+      // 处理不支持的方法
+      res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+      break;
+  }
 }
