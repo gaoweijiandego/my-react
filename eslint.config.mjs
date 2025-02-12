5099b77fd7ba1a0ef3ee287083ecfa355e5de9d1
NIKOFCUKINGTOP <2735977754@qq.com>
2025-02-12 14:34:31 +0800

    代码

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,26 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  {
+    ...compat.extends("next/core-web-vitals")[0],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: ['@next/next'],
+    settings: {
+      react: {
+        version: 'detect'
+      },
+    },
+    extends: [
+      'plugin:@next/next/recommended',
+      'next/core-web-vitals'
+    ],
+  },
+];
 
 export default eslintConfig;
